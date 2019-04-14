38d0fdebf9ae8d1e61115cec2a0e5fef538dbdbc
fredmontet <fredmontet@gmail.com>
2019-04-14 23:21:47 +0200

    Add JS env variables for Yarn

---
+++ javascript/env.zsh
@@ -0,0 +1,10 @@
+# Yarn JS package manager 
+# icu4c (one of yarn's dependencies)
+# If you need to have icu4c first in your PATH run:
+export PATH="/usr/local/opt/icu4c/bin:$PATH"
+export PATH="/usr/local/opt/icu4c/sbin:$PATH"
+# For compilers to find icu4c you may need to set:
+export LDFLAGS="-L/usr/local/opt/icu4c/lib"
+export CPPFLAGS="-I/usr/local/opt/icu4c/include"
+# For pkg-config to find icu4c you may need to set:
+export PKG_CONFIG_PATH="/usr/local/opt/icu4c/lib/pkgconfig"
