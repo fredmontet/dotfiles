bf370c7906b7238e0a4fe03c96cc4feb0b7a3c0e
Fred Montet <fredmontet@gmail.com>
2020-12-10 16:59:33 +0100

    remove vast.ch

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,20 +6,23 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-
-# Remove `+` and `-` from start of diff lines; just rely upon color.
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
-
+# Committing
 alias ga='git add -A'
-alias gc='git commit -mdot'
+alias gc='git commit -m'
 alias gca='git commit -a'
+alias gac='git add -A && git commit -m'
+
+# Branching
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
+
+# Pushing
+alias push='git push origin HEAD'
+alias pull='git pull --prune'
+
+# Status
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
+alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r' # remove `+` and `-` from start of diff lines; just rely upon color.
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias ge='git-edit-new'
