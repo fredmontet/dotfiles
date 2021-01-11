147d2c3b234afe29427526960790671960152778
Fred Montet <fredmontet@gmail.com>
2021-01-11 09:19:27 +0100

    add alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,7 +6,9 @@ alias ...='cd ../..'
 alias cls='clear' # Good 'ol Clear Screen command
 
 # Projects
-alias timeatlas='cd ~/src/heia-fr/timeatlas'
-alias fa4='cd ~/src/heia-fr/facility-4.0'
-alias as4='cd ~/src/heia-fr/assainissement-4.0'
+alias thesis='cd ~/src/heia-fr/thesis-report/ && code .'
+alias aa='cd ~/src/heia-fr/audit-arena && pipenv shell'
+alias ta='cd ~/src/heia-fr/timeatlas && pipenv shell'
+alias fa4='cd ~/src/heia-fr/facility-4.0 && pipenv shell'
+alias as4='cd ~/src/heia-fr/assainissement-4.0 && pipenv shell'
 alias vast='cd ~/src/vast-outdoor/vast-ch'
