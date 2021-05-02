af2eab6f0e119cce1a89b5d50e4690c5f670d0b5
Frédéric Montet <frederic.montet@vast.ch>
2021-05-02 11:28:31 +0200

    changed editor and clean

--- system/env.zsh
+++ system/env.zsh
@@ -1,22 +1,2 @@
-export EDITOR='subl'
+export EDITOR='code'
 export ENVIRONMENT='development'
-
-
-# Openblas
-
-## For compilers to find openblas you may need to set:
-#export LDFLAGS="-L/usr/local/opt/openblas/lib"
-#export CPPFLAGS="-I/usr/local/opt/openblas/include"
-
-## For pkg-config to find openblas you may need to set:
-#export PKG_CONFIG_PATH="/usr/local/opt/openblas/lib/pkgconfig"
-
-
-# Lapack
-
-## For compilers to find lapack you may need to set:
-#export LDFLAGS="-L/usr/local/opt/lapack/lib"
-#export CPPFLAGS="-I/usr/local/opt/lapack/include"
-
-## For pkg-config to find lapack you may need to set:
-#export PKG_CONFIG_PATH="/usr/local/opt/lapack/lib/pkgconfig"
